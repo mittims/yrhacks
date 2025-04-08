070c55a5c76a5186ed486d33ba54b150ccb1b150
Chester <chesterwzwang@gmail.com>
2025-04-08 09:23:24 -0400

    Added movable class

---
+++ src/Movable.java
@@ -0,0 +1,9 @@
+public class Movable {
+    public int x;
+    public int y;
+
+    public Movable (int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
--- src/Player.java
+++ src/Player.java
@@ -1,10 +1,15 @@
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-public class Player {
+public class Player extends Movable{
     public BufferedImage sprite;
     public int x;
     public int y;
     public Rectangle hitBox;
 
+    public int speed;
+
+    public Player (int x, int y){
+        super(x, y);
+    }
 }
