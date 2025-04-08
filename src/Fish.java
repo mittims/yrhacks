f7eb28241fe82190c1328418f6cc6b4dd56a5b1e
Chester <chesterwzwang@gmail.com>
2025-04-08 09:53:34 -0400

    Update

--- src/Fish.java
+++ src/Fish.java
@@ -5,11 +5,19 @@ public class Fish extends Movable{
 
     public BufferedImage fishSprite;
     public Rectangle fishHitBox;
-    public Fish (int x, int y, int speed){
+    public int changeFrame = 10;
+    public int localFrame;
+    public Fish (int x, int y, int speed) {
         super(x, y, speed);
+        localFrame = 0;
     }
-
     public void move (){
-
+        localFrame++;
+        x += speed * Math.cos(direction);
+        y += speed * Math.sin(direction);
+        if (localFrame == changeFrame) {
+            direction = Math.random()*360;
+            changeFrame = (int) (Math.random()*1000);
+        }
     }
 }
